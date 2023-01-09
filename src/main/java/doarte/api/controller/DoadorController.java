0d481db09b14f9f713b9eb82b10677896d37f7b3
LucasRPorto <lucasrodriguesporto@gmail.com>
2023-01-09 20:37:52 -0300

    feat(config-pageable): Lucas Porto

--- src/main/java/doarte/api/controller/DoadorController.java
+++ src/main/java/doarte/api/controller/DoadorController.java
@@ -6,6 +6,9 @@ import doarte.api.doador.Doador;
 import doarte.api.doador.DoadorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +24,13 @@ public class DoadorController {
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastrosDoador dados){
-
         repository.save(new Doador(dados));
     }
 
-    @GetMapping
-    public List<DadosListagemDoador> listarDoador() {
-        // Convertendo lista de doador para lista de dados listagem doador (DTO)
-        return repository.findAll().stream().map(DadosListagemDoador::new).toList();
+    @GetMapping // Não precisa de transactional pois estou apenas listando
+    public Page<DadosListagemDoador> listarDoador(@PageableDefault(sort = {"nome"})Pageable paginacao) {
+        // Convertendo lista de doador para lista de dados listagem doador (DTO) e motando esquema de paginação
+        return repository.findAll(paginacao).map(DadosListagemDoador::new);
     }
 
 }
